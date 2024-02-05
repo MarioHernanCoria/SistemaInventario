1c7c3b26d523ea5487020a69ee95b230d79d7927
MarioHernanCoria <mariocoria025@gmail.com>
2024-02-05 18:29:34 -0300

    Configuracion del Proyecto

--- SistemaInventario.AccesoDatos/Data/SistemaInventarioContext.cs
+++ SistemaInventario.AccesoDatos/Data/SistemaInventarioContext.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SistemaInventario.Modelos;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
 
 namespace SistemaInventario.AccesoDatos.Data;
 
@@ -11,11 +14,15 @@ public class SistemaInventarioContext : IdentityDbContext
     {
     }
 
+
+    public DbSet <Bodega> Bodegas { get; set; }
+
+
+
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-        // Customize the ASP.NET Identity model and override the defaults if needed.
-        // For example, you can rename the ASP.NET Identity table names and more.
-        // Add your customizations after calling base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
     }
 }
---
+++ SistemaInventario.Modelos/Bodega.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nombre es Requerido")]
+        [MaxLength(60, ErrorMessage ="Nombre debe ser maximo 60 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Descripcion es Requerido")]
+        [MaxLength(60, ErrorMessage = "Descripcion debe ser maximo 100 caracteres")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage ="Estado Requerido")]
+        public bool Estado { get; set; }
+    }
+}
